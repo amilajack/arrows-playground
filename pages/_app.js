691e9bbd71805f825e462f6aa274b38dee29bd6d
Amila Welihinda <amilajack@gmail.com>
2022-05-09 16:42:14 -0700

    chore: bump yarn, update deps

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    init({
+      key: "cl7nwhwbv004609jt5e7ybyk3",
+      plugins: [events(), vitals(), network(), measure(), profiler()],
+    });
+    profiler.start({
+      sampleInterval: 10,
+      maxBufferSize: 10_000,
+    });
+    addEventListener("load", () => {
+      profiler.stop();
+    });
+  }, []);
   return <Component {...pageProps} />;
 }
 
